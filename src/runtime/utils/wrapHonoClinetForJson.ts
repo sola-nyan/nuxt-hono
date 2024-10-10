fbf3903a137b7a56003238f4fe702463f9755cef
Solanyan <solanyan.net@gmail.com>
2024-10-10 17:01:46 +0900

    fix: improve typing

--- src/runtime/utils/wrapHonoClinetForJson.ts
+++ src/runtime/utils/wrapHonoClinetForJson.ts
@@ -1,11 +1,12 @@
 import type { ClientResponse } from 'hono/client'
 import type { ResponseFormat } from 'hono/types'
+import type { StatusCode } from 'hono/utils/http-status'
 
-export function wrapHonoClinetForJson<C>(hcClient: C) {
+export function wrapHonoClinet<C>(hcClient: C) {
   async function internalCaller<
     T extends Promise<ClientResponse<X, Y, ResponseFormat>>,
     X,
-    Y extends number,
+    Y extends StatusCode,
   >(
     caller: (client: typeof hcClient) => T,
   ) {
