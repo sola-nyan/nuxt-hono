13fc6fe71eced5702b8ef83b041ff0a1ee5e71b4
Solanyan <solanyan.net@gmail.com>
2024-10-10 17:02:07 +0900

    fix: change func name

--- src/runtime/utils/wrapHonoClinetForJson.ts
+++ src/runtime/utils/wrapHonoClinetForJson.ts
@@ -2,7 +2,7 @@ import type { ClientResponse } from 'hono/client'
 import type { ResponseFormat } from 'hono/types'
 import type { StatusCode } from 'hono/utils/http-status'
 
-export function wrapHonoClinet<C>(hcClient: C) {
+export function honoClientWrapper<C>(hcClient: C) {
   async function internalCaller<
     T extends Promise<ClientResponse<X, Y, ResponseFormat>>,
     X,
