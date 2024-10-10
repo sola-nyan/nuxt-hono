b6eb62b447829338f5d3b08698c60d85084e5d42
Solanyan <solanyan.net@gmail.com>
2024-10-10 18:03:40 +0900

    fix: revert

--- src/runtime/utils/honoClientWrapper.ts
+++ src/runtime/utils/honoClientWrapper.ts
@@ -3,20 +3,12 @@ import type { ResponseFormat } from 'hono/types'
 import type { StatusCode } from 'hono/utils/http-status'
 
 export function honoClientWrapper<C>(hcClient: C) {
-  async function internalCaller<T extends Promise<ClientResponse<X, Y, ResponseFormat>>, X, Y extends StatusCode>(caller: (client: typeof hcClient) => T): Promise<{
-    ok: boolean
-    status: Y
-    body: ReturnType<typeof $raw['json']>
-    $raw: Awaited<T>
-  }> {
-    const $raw = await caller(hcClient)
-    const body = await $raw.json() as ReturnType<typeof $raw['json']>
-    return {
-      ok: $raw.ok,
-      status: $raw.status,
-      body,
-      $raw,
-    }
+  async function internalCaller<
+    T extends Promise<ClientResponse<X, Y, ResponseFormat>>,
+    X,
+    Y extends StatusCode,
+  >(caller: (client: typeof hcClient) => T): Promise<Awaited<T>> {
+    return await caller(hcClient)
   }
 
   return internalCaller
