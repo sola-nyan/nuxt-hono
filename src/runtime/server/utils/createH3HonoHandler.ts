792103fd35e35e02108eb7e439df33f250be7dcc
Solanyan <solanyan.net@gmail.com>
2025-01-15 15:55:59 +0900

    fix: h3 handler create method no return

--- src/runtime/server/utils/createH3HonoHandler.ts
+++ src/runtime/server/utils/createH3HonoHandler.ts
@@ -3,7 +3,7 @@ import type { Hono } from 'hono'
 
 export function createH3HonoHandler<T extends Hono>(app: T) {
   const handler = defineEventHandler(async (event) => {
-    await app.fetch(toWebRequest(event), { event })
+    return await app.fetch(toWebRequest(event), { event })
   })
 
   return handler
