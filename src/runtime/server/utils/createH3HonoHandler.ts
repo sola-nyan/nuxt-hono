66e10bb8a153154f0a39f20e2973114a1202d044
Solanyan <solanyan.net@gmail.com>
2025-04-03 16:14:17 +0900

    add: handler proxy

--- src/runtime/server/utils/createH3HonoHandler.ts
+++ src/runtime/server/utils/createH3HonoHandler.ts
@@ -1,8 +1,8 @@
 import { defineEventHandler, toWebRequest } from 'h3'
 import type { Hono } from 'hono'
 
-export function createH3HonoHandler<T extends Hono>(app: T) {
-  const handler = defineEventHandler(async (event) => {
+export function createH3HonoHandler<T extends Hono>(app: T, proxyHandler = defineEventHandler) {
+  const handler = proxyHandler(async (event) => {
     return await app.fetch(toWebRequest(event), { event })
   })
 
