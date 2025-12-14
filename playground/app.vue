35e20af3061998c4973814b6a2350fc734825c78
Solanyan <solanyan.net@gmail.com>
2025-12-15 03:04:54 +0900

    add: comment

--- playground/app.vue
+++ playground/app.vue
@@ -1,14 +1,12 @@
 <!-- eslint-disable no-console -->
 <script setup lang="ts">
-import { hc } from 'hono/client'
 import type { AppType } from './server/api/[...]'
 
-const cc = hc<AppType>('http://localhost:3000')
 const client = createH3HonoClient<AppType>('http://localhost:3000', {
-  preHandler: (url) => {
+  preHandler: (url) => { // or async function
     console.log(`[pre] url: ${url}`)
   },
-  postHandler: (res) => {
+  postHandler: (res) => { // or async function
     console.log(`[post] ok: ${res.ok}`)
   },
 
